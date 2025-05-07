3310dca5aaf82a011c3286140f5a9cc1fd7160d4
Charu Gupta <Charu.Gupta@amantyatech.com>
2025-05-07 15:48:11 +0530

    Updated SumOfTwoNumber.java with better input handling

--- src/SumOfTwoNumber.java
+++ src/SumOfTwoNumber.java
@@ -1,9 +1,23 @@
 package src;
 
+import java.util.Scanner;
+
 public class SumOfTwoNumber {
+	public static int CalculateSum(int a, int b) {
+		int Sum = a + b;
+		return Sum;
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the FirstNumber : ");
+		int a = sc.nextInt();
+		System.out.println("Enter the SecondNumber : ");
+		int b = sc.nextInt();
+		int Sum = CalculateSum(a , b);
+		System.out.println("Sum of the two Number : " + Sum);
+		
 
 	}
 
